ec8666f9754de8dcdc271c39e341c1502317c4b9
vanshikavats23 <144532373+vanshikavats23@users.noreply.github.com>
2025-12-28 22:33:15 +0530

    js2

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const scrollableHeight=totalHeight-sectionHeight;
 
 window.addEventListener("scroll",()=>{
   const scrollTop=window.scrollY;
-//   const scrollPercent=(scrollTop/scrollableHeight)*100;
-//   para.forEach((p,index)=>{
-//     p.style.opacity=scrollPercent>index*20?1:0;
-//   });
-// });
+  const scrollPercent=(scrollTop/scrollableHeight)*100;
+  para.forEach((p,index)=>{
+    p.style.opacity=scrollPercent>index*20?1:0;
+  });
+});
 
 // document.addEventListener("DOMContentLoaded",()=>{
 //   para.forEach((p,index)=>{
