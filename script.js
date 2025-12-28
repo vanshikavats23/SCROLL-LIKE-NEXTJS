ea7166d9da44dd66fb06ce4d0145f3840078ac9f
vanshikavats23 <144532373+vanshikavats23@users.noreply.github.com>
2025-12-28 22:32:45 +0530

    js2

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const sectionHeight=section.offsetHeight;
 const totalHeight=section.scrollHeight;
 const scrollableHeight=totalHeight-sectionHeight;
 
-// window.addEventListener("scroll",()=>{
-//   const scrollTop=window.scrollY;
+window.addEventListener("scroll",()=>{
+  const scrollTop=window.scrollY;
 //   const scrollPercent=(scrollTop/scrollableHeight)*100;
 //   para.forEach((p,index)=>{
 //     p.style.opacity=scrollPercent>index*20?1:0;
