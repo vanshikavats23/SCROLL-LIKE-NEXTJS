d82ec652cfb4ebe219d336152c92b9a3c0901a11
vanshikavats23 <144532373+vanshikavats23@users.noreply.github.com>
2025-12-28 22:32:08 +0530

    js

---
+++ script.js
@@ -0,0 +1,19 @@
+const para=document.querySelectorAll("p");
+const section=document.querySelector("section");
+const sectionHeight=section.offsetHeight;
+const totalHeight=section.scrollHeight;
+const scrollableHeight=totalHeight-sectionHeight;
+
+// window.addEventListener("scroll",()=>{
+//   const scrollTop=window.scrollY;
+//   const scrollPercent=(scrollTop/scrollableHeight)*100;
+//   para.forEach((p,index)=>{
+//     p.style.opacity=scrollPercent>index*20?1:0;
+//   });
+// });
+
+// document.addEventListener("DOMContentLoaded",()=>{
+//   para.forEach((p,index)=>{
+//     p.style.opacity=0;
+//   });
+// }); 
